2ce55bed7f8dff9280c01b52c22d2c5ead5a8d5a
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-07-09 14:59:32 +0900

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classNames from "classnames";
+import "./Button.scss";
+
+function Button({ children, size, color, outline, fullwidth, ...rest }) {
+  return (
+    <button
+      className={classNames("Button", size, color, { outline, fullwidth })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
+
+export default Button;
